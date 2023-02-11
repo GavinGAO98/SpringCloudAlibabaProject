1748eb2b3b8f4d5d5c4b9b49b17fa6ae615716b9
Gavin <805792578@qq.com>
2023-02-11 22:40:13 +0800

    Adding Spring Cloud Alibaba Nacos Config

---
+++ cloudAlibaba-sentinel-service8401/src/main/java/com/gavin/cloud/SentinelServiceMain8401.java
@@ -0,0 +1,11 @@
+package com.gavin.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelServiceMain8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelServiceMain8401.class, args);
+    }
+}
